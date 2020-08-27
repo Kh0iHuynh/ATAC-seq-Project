669105fa99a17d2c1b30a8c41777a6e8d1030050
KhoiHuynh-thorntonlab <srongthiencos@gmail.com>
2020-08-27 08:23:46 -0700

    Update averagepeak.sh

--- averagepeak.sh
+++ averagepeak.sh
@@ -3,18 +3,17 @@
 module load bedtools/2.25.0
 module load R/3.4.1
 
-cat *.narrowPeak > drlongtempcat.txt
-awk -v OFS='\t' '{print $1, $2, $3, $7, $8,int($2+$10)}' drlongtempcat.txt > drlongtempawk.txt
-sort -k1,1 -k2,2n drlongtempawk.txt > drlongtempsort.txt
-bedtools merge  -c 4,5,6 -o mean,mean,collapse -i drlongtempsort.txt | sed 's/,/\t/g' > drlongtempmerge.bed
-#cat drlongtempmerge.bed | awk '{print $1,$2,$3,".\t0\t.\t",$4,$5,"-1\t",$6-$2}' > /bio/khoih/drlong/drlongtempmerge.peaks.bed
-rm drlongtempcat.txt drlongtempawk.txt drlongtempsort.txt
+cat *.narrowPeak > tempcat.txt
+awk -v OFS='\t' '{print $1, $2, $3, $7, $8,int($2+$10)}' tempcat.txt > tempawk.txt
+sort -k1,1 -k2,2n tempawk.txt > tempsort.txt
+bedtools merge  -c 4,5,6 -o mean,mean,collapse -i tempsort.txt | sed 's/,/\t/g' > tempmerge.bed
+
 
 ###############
 # Then fill the column with NA to make all row having the same number of column
-max=$(awk 'max < NF { max = NF } END { print max }'  drlongtempmerge.bed)
+max=$(awk 'max < NF { max = NF } END { print max }'  tempmerge.bed)
 
-awk -v max=$max 'BEGIN{OFS=FS="\t"}{ for(i=NF+1; i<=max; i++) $i = "NA"; print }'  drlongtempmerge.bed > tempwithna.txt
+awk -v max=$max 'BEGIN{OFS=FS="\t"}{ for(i=NF+1; i<=max; i++) $i = "NA"; print }'  tempmerge.bed > tempwithna.txt
 
 
 ######
@@ -32,14 +31,16 @@ Rscript getminmax.r
 
 #######
 ### scan for summit that is within (-500,500) from min or max
+###other.txt contains peak summits that are not within (-500,500) from min or max
+###other.txt is then renamed to tempawk.txt and the script is run against from line 8 until we get an empty other.txt
 ######
 
-# if max is within 1kb from min, col 66 = col 67 and peaks are overlapped and averaged.
-# if not, peaks are separated into 2 files based on distance to min, max
 
-for i in {6..65};do
-        awk -v i="$i" '{if(($i-$66 >= -500 && $i-$66 <=500) && ($i != "NA")) print $1,$2,$3,$4,$5,$i}' temp.txt >> min.txt
-        awk -v i="$i" '{if(($i-$67 >= -500 && $i-$67 <=500) && ($i != "NA") && ($66 != $67)) print $1,$2,$3,$4,$5,$i}' temp.txt >> max.txt
+
+for i in {6..16};do
+        awk -v i="$i" '{if(($i-$17 >= -500 && $i-$17 <=500) && ($i != "NA")) print $1,$2,$3,$4,$5,$i}' temp.txt >> min.txt
+        awk -v i="$i" '{if(($i-$18 >= -500 && $i-$18 <=500) && ($i != "NA") && ($17 != $18)) print $1,$2,$3,$4,$5,$i}' temp.txt >> max.txt
+        awk -v i="$i" '{if(($i-$18 <= -500 ) && ($i-$17 >=500) && ($i != "NA") && ($17 != $18)) print $1,$2,$3,$4,$5,$i}' temp.txt >> other.txt
 done
 
 awk -v OFS='\t' '{print $1, $2, $3, $4, $5, $6}' min.txt > mintemp.txt
@@ -47,10 +48,8 @@ sort -k1,1 -k2,2n mintemp.txt > mintempsort.txt
 bedtools merge  -c 4,5,6 -o mean  -i mintempsort.txt > minfinal.txt
 cat minfinal.txt | awk -v OFS="\t" '{print $1,$2,$3,"peak","1000",".",$4,$5,"-1",int($6-$2)}' > min.peaks.bed
 
-
 awk -v OFS='\t' '{print $1, $2, $3, $4, $5, $6}' max.txt > maxtemp.txt
 sort -k1,1 -k2,2n maxtemp.txt > maxtempsort.txt
 bedtools merge  -c 4,5,6 -o mean  -i maxtempsort.txt > maxfinal.txt
 cat maxfinal.txt | awk -v OFS="\t" '{print $1,$2,$3,"peak","1000",".",$4,$5,"-1",int($6-$2)}' > max.peaks.bed
-
-cat min.peaks.bed max.peaks.bed > final.narrowPeak
+cat min.peaks.bed max.peaks.bed >> final.narrowPeak
