07bda5403d78639b995162bebb26db4c828b7740
KhoiHuynh-thorntonlab <srongthiencos@gmail.com>
2020-08-27 08:27:38 -0700

    Create CLmatrix.sh

---
+++ CLmatrix.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#$ -N testrun
+#$ -q bio,abio*,free*
+#$ -pe openmp 48
+#$ -R y
+#$ -t 1-96
+#$ -ckpt blcr
+
+
+newdata="/dfs5/bio/khoih/euonly"
+files="./name.txt"
+rawfix=`head -n $SGE_TASK_ID $files | tail -n 1`
+
+module purge
+module load khoih/anaconda/3
+source activate test
+
+###
+# rawfix is the name of 96 samples
+###
+mkdir $rawfix
+rm $rawfix/matrix*
+sort -k1,1 -k2,2n ./fragfiles/$rawfix.withweight.txt > $rawfix.test.txt
+bedtools intersect -sorted -a final2.bed -b $rawfix.test.txt -wa -wb > temp.$rawfix.txt
+awk 'BEGIN { OFS = "\t"}{print $4,$5,$6,$7,$8,$1,$2,$3}' temp.$rawfix.txt > $rawfix.withweight.txt
+
+
+python final.py -b final2 -f $rawfix
