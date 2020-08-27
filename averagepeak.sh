03a2bd1157b6461759b948e8a58a75eba83595f5
KhoiHuynh-thorntonlab <srongthiencos@gmail.com>
2020-08-27 08:16:45 -0700

    Update averagepeak.sh

--- averagepeak.sh
+++ averagepeak.sh
@@ -28,3 +28,29 @@ echo 'data[,67] <- NULL' >> getminmax.r
 echo 'write.table(data, file = "temp.txt", append = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE,quotes =FALSE)' >> getminmax.r
 
 Rscript getminmax.r
+
+
+#######
+### scan for summit that is within (-500,500) from min or max
+######
+
+# if max is within 1kb from min, col 66 = col 67 and peaks are overlapped and averaged.
+# if not, peaks are separated into 2 files based on distance to min, max
+
+for i in {6..65};do
+        awk -v i="$i" '{if(($i-$66 >= -500 && $i-$66 <=500) && ($i != "NA")) print $1,$2,$3,$4,$5,$i}' temp.txt >> min.txt
+        awk -v i="$i" '{if(($i-$67 >= -500 && $i-$67 <=500) && ($i != "NA") && ($66 != $67)) print $1,$2,$3,$4,$5,$i}' temp.txt >> max.txt
+done
+
+awk -v OFS='\t' '{print $1, $2, $3, $4, $5, $6}' min.txt > mintemp.txt
+sort -k1,1 -k2,2n mintemp.txt > mintempsort.txt
+bedtools merge  -c 4,5,6 -o mean  -i mintempsort.txt > minfinal.txt
+cat minfinal.txt | awk -v OFS="\t" '{print $1,$2,$3,"peak","1000",".",$4,$5,"-1",int($6-$2)}' > min.peaks.bed
+
+
+awk -v OFS='\t' '{print $1, $2, $3, $4, $5, $6}' max.txt > maxtemp.txt
+sort -k1,1 -k2,2n maxtemp.txt > maxtempsort.txt
+bedtools merge  -c 4,5,6 -o mean  -i maxtempsort.txt > maxfinal.txt
+cat maxfinal.txt | awk -v OFS="\t" '{print $1,$2,$3,"peak","1000",".",$4,$5,"-1",int($6-$2)}' > max.peaks.bed
+
+cat min.peaks.bed max.peaks.bed > final.narrowPeak
