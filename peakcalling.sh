9d02df4cf6a97d6e0138387d13bbf6d7b7d30e72
KhoiHuynh-thorntonlab <srongthiencos@gmail.com>
2020-08-27 08:07:27 -0700

    Update peakcalling.sh

--- peakcalling.sh
+++ peakcalling.sh
@@ -10,6 +10,9 @@ module load enthought_python/7.3.2
 module load python/2.7.2
 
 files="/dfs3/bio/khoih/tissuelist.txt"
+#tissuelist.txt is file contains 4 tissue name "BR","OV","ED","WD"
 rawfix=`head -n $SGE_TASK_ID $files | tail -n 1`
 
-#macs2 callpeak -t /dfs3/bio/khoih/backup2/all.temp.${rawfix}.corrected.bam -f BAMPE -n justbampe.${rawfix} -g 142573024 -p 0.01 --nomodel -B --SPMR --keep-dup all --call-summits
+samtools merge /dfs3/bio/khoih/backup2/all.${rawfix}.corrected.bam *BR*corrected.bam
+
+macs2 callpeak -t /dfs3/bio/khoih/backup2/all.${rawfix}.corrected.bam -f BAMPE -n justbampe.${rawfix} -g 142573024 -p 0.01 --nomodel -B --SPMR --keep-dup all --call-summits
