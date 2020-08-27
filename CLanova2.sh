cf32e45b681690850bcbf1ae356ece027ab0bff6
KhoiHuynh-thorntonlab <srongthiencos@gmail.com>
2020-08-27 08:57:56 -0700

    Update CLanova2.sh

--- CLanova2.sh
+++ CLanova2.sh
@@ -9,10 +9,16 @@ module load bedtools/2.25.0
 module load R/3.4.1
 
 
-
-
 echo "chr     pos     C       L       genotype        tissue  replicate" > CLtable.txt
-
 cat *.temp3.txt >> CLtable.txt
-
 Rscript CLanova.r
+#####
+# combine all results into 1 result file
+####
+head -n 1 test.txt > test3.txt
+sort -k 1,1 -k2,2n test.txt >> test3.txt
+head -n 1 test2.txt > test4.txt
+sort -k 1,1 -k2,2n test2.txt >> test4.txt
+paste -d" " test3.txt test4.txt > test5.txt
+awk '{ $11=$12=""; print}' test5.txt > result.txt
+
