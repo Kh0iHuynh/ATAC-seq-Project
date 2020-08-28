2b8ab1c564c056feb305e6fb50f65822cedbb2bb
Kh0iHuynh <srongthiencos@gmail.com>
2020-08-28 05:15:13 -0700

    Update peakcalling.sh

--- peakcalling.sh
+++ peakcalling.sh
@@ -13,6 +13,6 @@ files="/dfs3/bio/khoih/tissuelist.txt"
 #tissuelist.txt is file contains 4 tissue name "BR","OV","ED","WD"
 rawfix=`head -n $SGE_TASK_ID $files | tail -n 1`
 
-samtools merge /dfs3/bio/khoih/backup2/all.${rawfix}.corrected.bam *BR*corrected.bam
+samtools merge /dfs3/bio/khoih/backup2/all.${rawfix}.corrected.bam *${rawfix}*corrected.bam
 
 macs2 callpeak -t /dfs3/bio/khoih/backup2/all.${rawfix}.corrected.bam -f BAMPE -n justbampe.${rawfix} -g 142573024 -p 0.01 --nomodel -B --SPMR --keep-dup all --call-summits
