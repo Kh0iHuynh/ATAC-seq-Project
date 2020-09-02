caf753bf9af2247cc84a7cbcd7174a4666edfb12
Kh0iHuynh <srongthiencos@gmail.com>
2020-09-01 19:57:34 -0700

    Update peakqualityplot.r

--- peakqualityplot.r
+++ peakqualityplot.r
@@ -16,6 +16,7 @@ library(scales)
 # percent frequency = frequency/ total frequency.
 # The out put of this steps are used for Peak enrichment histogram plot
 ######
+
 BR <- read.table("BRpeaks.txt",header = TRUE)
 BR$rounded <- round(BR,digits=0)
 BR2 <- count(BR$rounded)
@@ -41,6 +42,7 @@ WD2$percent <- WD2$freq/sum(WD2$freq)
 # These inputs are used to generate peak enrichment vs distance to TSS plot
 # and peak enrichment vs annotation feature type plot
 ######
+
 brain2 <- read.table("BRheightbyfeature.txt", header =TRUE)
 ovary2 <- read.table("OVheightbyfeature.txt", header =TRUE)
 eye2 <- read.table("EDheightbyfeature.txt", header =TRUE)
@@ -66,9 +68,11 @@ data2 <- read.table("score.txt",header = TRUE, sep = "\t")
 data3 <- read.table("peakcount.txt",header = TRUE, sep =" ")
 
 frip<-ggplot(data=data2, aes(x=Tissue, y=FRiP)) + geom_bar(stat="identity") + ggtitle("i/ FRiP score") + theme_classic()
+
 ######
 # peak enrichment histogram plots for each tissue
 ######
+
 BRhist <- ggplot(data=BR2, aes(x=PeakScore, y=percent)) + geom_bar(stat="identity") + ggtitle("Brain Peak Enrichment Histogram") + ylab("Frequency") + xlab("Fold Enrichment") + xlim(0,13) + ylim(0,0.5) + theme_classic()
 OVhist <- ggplot(data=OV2, aes(x=PeakScore, y=percent)) + geom_bar(stat="identity") + ggtitle("Ovary Peak Enrichment Histogram") + ylab("Frequency") + xlab("Fold Enrichment") + xlim(0,13) + ylim(0,0.5) + theme_classic()
 EDhist <- ggplot(data=ED2, aes(x=PeakScore, y=percent)) + geom_bar(stat="identity") + ggtitle("Eye Disc Peak Enrichment Histogram") + ylab("Frequency") + xlab("Fold Enrichment") + xlim(0,13) + ylim(0,0.5) + theme_classic()
@@ -79,6 +83,7 @@ peakcount<-ggplot(data3, aes(x=Tissue, y=Count)) + geom_bar(stat="identity") + g
 ######
 # generate peak ennrichment by feature types for each tissue
 ######
+
 brainplot <- ggplot(brain2, aes(x=brain2$Annotation, y=brain2$PeakScore)) + geom_boxplot() + ggtitle("a/Peak enrichment by feature \n for Brain") + ylim(0,12) + xlab("Annotation") + ylab("Fold Enrichment") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(),axis.line = element_line(colour = "black"), axis.text.x = element_text(angle = 90, hjust = 1))
 ovaryplot <- ggplot(ovary2, aes(x=ovary2$Annotation, y=ovary2$PeakScore)) + geom_boxplot() + ggtitle("b/Peak enrichment by feature \n for Ovary") + ylim(0,12) + xlab("Annotation") + ylab("Fold Enrichment") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(),axis.line = element_line(colour = "black"),axis.text.x = element_text(angle = 90, hjust = 1))
 eyeplot <- ggplot(eye2, aes(x=eye2$Annotation, y=eye2$PeakScore)) + geom_boxplot() + ggtitle("c/Peak enrichment by feature \n for Eye Disc") + ylim(0,12) + xlab("Annotation") + ylab("Fold Enrichment") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(),axis.line = element_line(colour = "black"),axis.text.x = element_text(angle = 90, hjust = 1))
@@ -88,6 +93,7 @@ wingplot <- ggplot(wing2, aes(x=wing2$Annotation, y=wing2$PeakScore)) + geom_box
 ######
 # generate peak enrichment by distance to trasncription starting site TSS
 ######
+
 tssbrain <- ggplot(brain2, aes(x=brain2$DistancetoTSS, y=brain2$PeakScore)) + geom_point(size = 0.3) + ylim(0,12) + ggtitle("e/Peak enrichment by distance \n to TSS for Brain") + xlab("Distance to TSS") + ylab("Fold Enrichment") + theme_classic()
 tssovary <- ggplot(ovary2, aes(x=ovary2$DistancetoTSS, y=ovary2$PeakScore)) + geom_point(size = 0.3) + ylim(0,12) + ggtitle("f/Peak enrichment by distance \n to TSS for Ovary") + xlab("Distance to TSS") + ylab("Fold Enrichment") + theme_classic()
 tsseye <- ggplot(eye2, aes(x=eye2$DistancetoTSS, y=eye2$PeakScore)) + geom_point( size = 0.3) + ylim(0,12) + ggtitle("g/Peak enrichment by distance \n to TSS for Eye Disc") + xlab("Distance to TSS") + ylab("Fold Enrichment") + theme_classic()
@@ -95,11 +101,10 @@ tsswing <- ggplot(wing2, aes(x=wing2$DistancetoTSS, y=wing2$PeakScore)) + geom_p
 
 
 
-offsets = data.frame(chrom=c("chrX","chr2L","chr2R","chr3L","chr3R"),off=cumsum(c(0,23542271,23513712,25286936,28110227)))
-offsetPerPosbrain = brain$pos + offsets[match(brain$chr,offsets$chr),2]
-offsetPerPosovary = ovary$pos + offsets[match(ovary$chr,offsets$chr),2]
-offsetPerPoseye = eye$pos + offsets[match(eye$chr,offsets$chr),2]
-offsetPerPoswing = wing$pos + offsets[match(wing$chr,offsets$chr),2]
+######
+# venn diagram plots to compared number of peaks overlapped between tissues 
+# for peaks with -log(pvalue) >=2 and -log(pvalue) >=3 
+######
 
 log102venn.plot <- draw.quad.venn(area1 = 17162, area2 = 13107, area3 = 13803, area4 = 13811, n12 = 3023, n13 = 3138, n14 = 3216, n23 = 7937, n24 = 4029,n34 = 4043, n123 = 2568 , n124 = 2045, n134 = 2065, n234 = 3361, n1234 = 1856, category = c("Brain", "Wing", "Eye","Ovary"),lty = "solid",fill = c("skyblue", "pink1","mediumorchid", "orange"), main = "Overlapped Peaks Between Tissues")
 
@@ -110,11 +115,13 @@ representative <- readJPEG('representive_region_chr3L_8,441,177_8,790,676.jpg')
 representativezoom <- readJPEG('representive_region_chr3L_8,577,094_8,654,760.jpg')
 representativezoom2 <- readJPEG('representive_region_chr3L_8,598,462_8,607,091.jpg')
 
+######
+# manhattan plot scripts for log(p-value) and peak enrichment
+######
 brainman <- ggplot(brain, aes(poskb, logp)) + geom_point(size=0.1) + facet_grid(. ~ chr) + stat_smooth(aes(y=logp, x=poskb), method = lm, formula = y ~ poly(x, 10), se = FALSE) + ylim(0,15000) + ggtitle("Manhattan -log10(p-value) Brain") + xlab("Position (Mb)") + ylab("-log10(p-value)") + theme_classic()
 ovaryman <- ggplot(ovary, aes(poskb, logp)) + geom_point(size=0.1) + facet_grid(. ~ chr) + stat_smooth(aes(y=logp, x=poskb), method = lm, formula = y ~ poly(x, 10), se = FALSE) + ylim(0,15000) + ggtitle("Manhattan -log10(p-value) Ovary") + xlab("Position (Mb)") + ylab("-log10(p-value)") + theme_classic()
 eyeman <- ggplot(eye, aes(poskb, logp)) + geom_point(size=0.1) + facet_grid(. ~ chr) + stat_smooth(aes(y=logp, x=poskb), method = lm, formula = y ~ poly(x, 10), se = FALSE) + ylim(0,15000) + ggtitle("Manhattan -log10(p-value) Eye Disc") + xlab("Position (Mb") + ylab("-log10(p-value)") + theme_classic()
 wingman <- ggplot(wing, aes(poskb, logp)) + geom_point(size=0.1) + facet_grid(. ~ chr) + stat_smooth(aes(y=logp, x=poskb), method = lm, formula = y ~ poly(x, 10), se = FALSE) + ylim(0,15000) + ggtitle("Manhattan -log10(p-value) Wing Disc") + xlab("Position (Mb)") + ylab("-log10(p-value)") + theme_classic()
-
 
 
 brainman2 <- ggplot(brain, aes(poskb, Fold)) + geom_point(size=0.1) + facet_grid(. ~ chr) + stat_smooth(aes(y=Fold, x=poskb), method = lm, formula = y ~ poly(x, 10), se = FALSE) + ylim(0,10) + ggtitle("Manhattan Fold Enrichment Brain") + xlab("Position (Mb)") + ylab("Fold Enrichment") + theme_classic()
@@ -126,17 +133,19 @@ wingman2 <- ggplot(wing, aes(poskb, Fold)) + geom_point(size=0.1) + facet_grid(.
 ######
 # generate pdf files and output all plots
 ######
+
 pdf("plot.pdf")
 
 grid.arrange(brainplot,ovaryplot,eyeplot,wingplot,nrow = 2)
 grid.arrange(frip,peakcount,gTree(children=log102venn.plot),gTree(children=log103venn.plot), nrow = 2)
 grid.arrange(BRhist,OVhist,EDhist,WDhist,nrow=2)
 grid.arrange(brainman,ovaryman,eyeman,wingman,nrow=4)
-
 grid.arrange(brainman2,ovaryman2,eyeman2,wingman2,nrow=4)
 
 
-
+######
+# plot peak enrichment vs distance to TSS of peaks
+######
 par(mfrow=c(2,2))
 with(brain2, {
     plot(brain2$DistancetoTSS, brain2$PeakScore, xlim=c(-40000,40000),pch=16, cex=0.1,ylim=c(0,10), xlab="DistancetoTSS",ylab="Fold-enrichment",main="Brain Peak Enrichment\nby Distance to TSS")
@@ -158,38 +167,9 @@ with(wing2, {
     lines(ksmooth(wing2$DistancetoTSS, wing2$PeakScore, "normal", bandwidth = 1000), col = 2)
 })
 
-
-par(mfrow=c(2,2))
-plot(offsetPerPosbrain/1e6,brain$logp,pch=".",main="Manhattan -log10(p-value) Brain",xlab="position (Mb)", ylab="-log10(p-value)",ylim=c(0,15000))
-for (i in 2:5){abline(v=offsets[i,2]/1e6,col="red")}
-
-
-plot(offsetPerPosovary/1e6,ovary$logp,pch=".",main="Manhattan -log10(p-value) Ovary",xlab="position (Mb)", ylab="-log10(p-value)",ylim=c(0,15000))
-for (i in 2:5){abline(v=offsets[i,2]/1e6,col="red")}
-
-plot(offsetPerPoseye/1e6,eye$logp,pch=".",main="Manhattan -log10(p-value) Eye Disc",xlab="position (Mb)", ylab="-log10(p-value)",ylim=c(0,15000))
-for (i in 2:5){abline(v=offsets[i,2]/1e6,col="red")}
-
-
-
-plot(offsetPerPoswing/1e6,wing$logp,pch=".",main="Manhattan -log10(p-value) Wing Disc",xlab="position (Mb)", ylab="-log10(p-value)",ylim=c(0,15000))
-for (i in 2:5){abline(v=offsets[i,2]/1e6,col="red")}
-
-
-par(mfrow=c(2,2))
-
-plot(offsetPerPosbrain/1e6,brain$Fold,pch=".",main="Manhattan Fold Enrichment Brain",xlab="position (Mb)", ylab="Fold-enrichment",ylim=c(0,10))
-for (i in 2:5){abline(v=offsets[i,2]/1e6,col="red")}
-
-plot(offsetPerPosovary/1e6,ovary$Fold,pch=".",main="Manhattan Fold Enrichment Ovary",xlab="position (Mb)", ylab="Fold-enrichment",ylim=c(0,10))
-for (i in 2:5){abline(v=offsets[i,2]/1e6,col="red")}
-
-plot(offsetPerPoseye/1e6,eye$Fold,pch=".",main="Manhattan Fold Enrichment Eye Disc",xlab="position (Mb)", ylab="Fold-enrichment",ylim=c(0,10))
-for (i in 2:5){abline(v=offsets[i,2]/1e6,col="red")}
-
-plot(offsetPerPoswing/1e6,wing$Fold,pch=".",main="Manhattan Fold Enrichment Wing Disc",xlab="position (Mb)", ylab="Fold-enrichment",ylim=c(0,10))
-for (i in 2:5){abline(v=offsets[i,2]/1e6,col="red")}
-
+#######
+# representative window from UCSC genome browser
+#######
 
 grid.arrange(rasterGrob(representative), rasterGrob(representativezoom),rasterGrob(representativezoom2),ncol=1, nrow =3, top ="Averaged Peaks at Different Zoom Level")
 
