0eeb7e8517e478582dc476d5761adf1396a0c1e9
Kh0iHuynh <srongthiencos@gmail.com>
2020-09-02 03:49:29 -0700

    Create anovaresultplot.r

---
+++ anovaresultplot.r
@@ -0,0 +1,56 @@
+library(ggplot2)
+library(gridExtra)
+library(VennDiagram)
+library(jpeg)
+library(plyr)
+library(scales)
+
+bigout <- read.table("result.txt",header = TRUE)
+bigout <- as.data.frame(bigout)
+bigout$poskb <- bigout$pos/1000000
+N=nrow(bigout)
+
+#bigout<- lapply(bigout, function(x) replace(x, is.nan(x), 0))
+# Q-Q plots...
+
+
+
+
+tissuec <- ggplot(bigout, aes(poskb,Clog10t)) + geom_point(size=0.1) + facet_grid(. ~ chr) + stat_smooth(aes(y=Clog10t, x=poskb), method = lm, formula = y ~ poly(x, 10), se = FALSE) + ylim(0,25) + ggtitle("Manhattan -log10(p-value) C Tissue") + xlab("Position (Mb)") + ylab("-log10(p-value)") + theme_classic()
+
+genotypec <- ggplot(bigout, aes(poskb,Clog10g)) + geom_point(size=0.1) + facet_grid(. ~ chr) + stat_smooth(aes(y=Clog10g, x=poskb), method = lm, formula = y ~ poly(x, 10), se = FALSE) + ylim(0,25) + ggtitle("Manhattan -log10(p-value) C Genotype") + xlab("Position (Mb)") + ylab("-log10(p-value)") + theme_classic()
+
+interactionc <- ggplot(bigout, aes(poskb,Clog10tbyg)) + geom_point(size=0.1) + facet_grid(. ~ chr) + stat_smooth(aes(y=Clog10tbyg, x=poskb), method = lm, formula = y ~ poly(x, 10), se = FALSE) + ylim(0,25) + ggtitle("Manhattan -log10(p-value) C Genotype:Tissue") + xlab("Position (Mb)") + ylab("-log10(p-value)") + theme_classic()
+
+
+tissuel <- ggplot(bigout, aes(poskb,Llog10t)) + geom_point(size=0.1) + facet_grid(. ~ chr) + stat_smooth(aes(y=Llog10t, x=poskb), method = lm, formula = y ~ poly(x, 10), se = FALSE) + ylim(0,25) + ggtitle("Manhattan -log10(p-value) L Tissue") + xlab("Position (Mb)") + ylab("-log10(p-value)") + theme_classic()
+
+genotypel <- ggplot(bigout, aes(poskb,Llog10g)) + geom_point(size=0.1) + facet_grid(. ~ chr) + stat_smooth(aes(y=Llog10g, x=poskb), method = lm, formula = y ~ poly(x, 10), se = FALSE) + ylim(0,25) + ggtitle("Manhattan -log10(p-value) L Genotype") + xlab("Position (Mb)") + ylab("-log10(p-value)") + theme_classic()
+
+interactionl <- ggplot(bigout, aes(poskb,Llog10tbyg)) + geom_point(size=0.1) + facet_grid(. ~ chr) + stat_smooth(aes(y=Llog10tbyg, x=poskb), method = lm, formula = y ~ poly(x, 10), se = FALSE) + ylim(0,25) + ggtitle("Manhattan -log10(p-value) L Genotype:Tissue") + xlab("Position (Mb)") + ylab("-log10(p-value)") + theme_classic()
+
+
+residc <- ggplot(bigout, aes(mC,residualC)) + geom_point(size=0.1) + facet_grid(. ~ chr) + theme_classic()
+residl <- ggplot(bigout, aes(mL,residualL)) + geom_point(size=0.1) + facet_grid(. ~ chr) + theme_classic()
+
+
+grid.arrange(residc,residl,nrow =1)
+attach(bigout)
+par(mfrow=c(2,2))
+
+plot(bigout$mC, bigout$residualC,pch=16, cex=0.1, ylim=c(0,1.5) ,xlab="mean log10(C)",ylab="Mean square residual",main="residual plot C")
+plot(sort(-log10(1:N/N)),sort(Clog10t),pch=".", main="QQ tissue (log10C)")
+plot(sort(-log10(1:N/N)),sort(Clog10g),pch=".", main="QQ genotype (log10C)")
+plot(sort(-log10(1:N/N)),sort(Clog10tbyg),pch=".", main="QQ tiss:geno (log10C)")
+detach(bigout)
+grid.arrange(tissuec,genotypec,interactionc, nrow = 2)
+
+attach(bigout)
+par(mfrow=c(2,2))
+plot(bigout$mL, bigout$residualL,pch=16, cex=0.1, xlab="mean L",ylab="Mean square residual",main="residual plot L")
+plot(sort(-log10(1:N/N)),sort(Llog10t),pch=".", main="QQ tissue (L)")
+plot(sort(-log10(1:N/N)),sort(Llog10g),pch=".", main="QQ genotype (L)")
+plot(sort(-log10(1:N/N)),sort(Llog10tbyg),pch=".", main="QQ tiss:geno (L)")
+detach(bigout)
+grid.arrange(tissuel,genotypel,interactionl, nrow = 2)
+
