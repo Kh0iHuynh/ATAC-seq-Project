81afe31b514ec4b4ec917f9640bf9bc3b5766bb5
Kh0iHuynh <srongthiencos@gmail.com>
2020-09-18 06:58:01 -0700

    Update peakannotation.sh

--- peakannotation.sh
+++ peakannotation.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 
-awk  -F'\t' 'BEGIN{OFS="\t";} {if($8 >= 2) print $4,$1,$2,$3,$6="+";}' ../onlyeu.OV_peaks.narrowPeak > OV.peakannotation.txt
+aawk  -F'\t' 'BEGIN{OFS="\t";} {if($8 >= 2) print $1,$2 + $10 - 1,$2 + $10 + 1,$4,$7,"+";}' ../../../onlyeu.OV_peaks.narrowPeak > OV.peakannotation.txt
 perl /dfs5/bio/khoih/homer/bin/annotatePeaks.pl OV.peakannotation.txt dm6 > OV.peakannotation.2.txt
 
 
 
-awk  -F'\t' 'BEGIN{OFS="\t";} {if($8>= 2) print $4,$1,$2,$3,$6="+";}' ../onlyeu.BR_peaks.narrowPeak > BR.peakannotation.txt
+awk  -F'\t' 'BEGIN{OFS="\t";} {if($8>= 2) print $1,$2 + $10 - 1,$2 + $10 + 1,$4,$7,"+";}' ../../../onlyeu.BR_peaks.narrowPeak > BR.peakannotation.txt
 perl /dfs5/bio/khoih/homer/bin/annotatePeaks.pl BR.peakannotation.txt dm6 > BR.peakannotation.2.txt
 
 
-awk  -F'\t' 'BEGIN{OFS="\t";} {if($8>= 2) print $4,$1,$2,$3,$6="+";}' ../onlyeu.WD_peaks.narrowPeak > WD.peakannotation.txt
+awk  -F'\t' 'BEGIN{OFS="\t";} {if($8>= 2) print $1,$2 + $10 - 1,$2 + $10 + 1,$4,$7,"+";}' ../../../onlyeu.WD_peaks.narrowPeak > WD.peakannotation.txt
 perl /dfs5/bio/khoih/homer/bin/annotatePeaks.pl WD.peakannotation.txt dm6 > WD.peakannotation.2.txt
 
 
 
-awk  -F'\t' 'BEGIN{OFS="\t";} {if($8>= 2) print $4,$1,$2,$3,$6="+";}' ../onlyeu.ED_peaks.narrowPeak > ED.peakannotation.txt
+awk  -F'\t' 'BEGIN{OFS="\t";} {if($8>= 2) print $1,$2 + $10 - 1,$2 + $10 + 1,$4,$7,"+";}' ../../../onlyeu.ED_peaks.narrowPeak > ED.peakannotation.txt
 perl /dfs5/bio/khoih/homer/bin/annotatePeaks.pl ED.peakannotation.txt dm6 > ED.peakannotation.2.txt
+
