2f05348c91ea21331643a4923a9677f5b53f9203
Kh0iHuynh <srongthiencos@gmail.com>
2020-12-12 06:57:08 -0800

    Create suppfig3worldmap.r

---
+++ suppfig3worldmap.r
@@ -0,0 +1,9 @@
+library(ggmap)
+library(maps) 
+library(maptools)
+
+#############
+# blank world map is created and data is filled in manually with gimp
+#############
+
+map("world",fill =TRUE,col = "white",bg ="white",ylim=c(-60,110))
