79439f72cc85a450ea91d9d41722a58617d0f24d
Kh0iHuynh <srongthiencos@gmail.com>
2021-01-05 09:53:43 -0800

    Update Coverageindex.sh

--- Coverageindex.sh
+++ Coverageindex.sh
@@ -3,24 +3,24 @@
 ######
 # print
 ######
-sort snpcovint.txt | uniq -c | awk '{ print $2,$3}' | sed 's/ /\t/g' > temp1.txt
+sort snpcovint.txt | uniq -c | awk '{ print $2,$3}' | sed 's/ /\t/g' > delete1.txt
 awk 'FNR==NR{a[$1] =$1;b[$2]= $2;next}($1 in a) && ($2 in b){ print $0;}' delete1.txt note.txt | sed 's/\t/ /g' > notesnpcovint.txt
 
 
-sort snpcovgeno.txt | uniq -c | awk '{ print $2,$3}' | sed 's/ /\t/g' > temp2.txt
+sort snpcovgeno.txt | uniq -c | awk '{ print $2,$3}' | sed 's/ /\t/g' > delete2.txt
 awk 'FNR==NR{a[$1] =$1;b[$2]= $2;next}($1 in a) && ($2 in b){ print $0;}' delete2.txt note.txt | sed 's/\t/ /g' > notesnpcovgeno.txt
 
 
 
 
-sort SVcovint.txt | uniq -c | awk '{ print $2,$3}' | sed 's/ /\t/g' > temp3.txt
+sort SVcovint.txt | uniq -c | awk '{ print $2,$3}' | sed 's/ /\t/g' > delete3.txt
 awk 'FNR==NR{a[$1] =$1;b[$2]= $2;next}($1 in a) && ($2 in b){ print $0;}' delete3.txt note.txt | sed 's/\t/ /g' > noteSVcovint.txt
 
 
-sort SVcovgeno.txt | uniq -c | awk '{ print $2,$3}' | sed 's/ /\t/g' > temp4.txt
+sort SVcovgeno.txt | uniq -c | awk '{ print $2,$3}' | sed 's/ /\t/g' > delete4.txt
 awk 'FNR==NR{a[$1] =$1;b[$2]= $2;next}($1 in a) && ($2 in b){ print $0;}' delete4.txt note.txt | sed 's/\t/ /g' > noteSVcovgeno.txt
 
 
 
 
-python Coverageindex.py
+python Coverageindex1.py
