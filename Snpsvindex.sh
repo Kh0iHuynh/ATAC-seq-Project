d52292f82e9af315cf1bb19bbd2b995874168824
Kh0iHuynh <srongthiencos@gmail.com>
2021-01-05 10:02:06 -0800

    Update Snpsvindex.sh

--- Snpsvindex.sh
+++ Snpsvindex.sh
@@ -55,37 +55,29 @@ python Lengthindex2.py
 ######
 
 
-
-#!/bin/bash
-
-
 echo "chr peak_pos marker_pos coverage geno tis rep marker" > scg.txt
 echo "chr peak_pos marker_pos coverage geno tis rep marker" > sci.txt
-#echo "chr peak_pos marker_pos coverage geno tis rep marker" > svcg.txt
-#echo "chr peak_pos marker_pos coverage geno tis rep marker" > svci.txt
-
-
 echo "chr peak_pos marker_pos length geno tis rep marker" > slg.txt
 echo "chr peak_pos marker_pos length geno tis rep marker" > sli.txt
-#echo "chr peak_pos marker_pos length geno tis rep marker" > svlg.txt
-#echo "chr peak_pos marker_pos length geno tis rep marker" > svli.txt
 
 
-sort coverage/scg.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n >> scg.txt
-sort coverage/sci.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n >> sci.txt
-sort coverage/svcg.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n > svcg.txt
-sort coverage/svci.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n > svci.txt
 
-sort length/svli.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n > svli.txt
-sort length/svlg.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n > svlg.txt
-sort length/sli.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n >> sli.txt
-sort length/slg.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n >> slg.txt
+sort scg2.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n >> scg.txt
+sort sci2.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n >> sci.txt
+sort svcg2.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n > svcg.txt
+sort svci2.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n > svci.txt
+
+sort svli2.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n > svli.txt
+sort svlg2.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n > svlg.txt
+sort sli2.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n >> sli.txt
+sort slg2.txt | uniq -c | awk '{print $2,$3,$4,$5,$6,$7,$8,$9}' | sort -k1,1 -k2,2n -k3,3n -k5,5 -k6,6 -k7,7n >> slg.txt
 
 rm tempsvli.txt
 rm tempsvlg.txt
 rm tempsvci.txt
 rm tempsvcg.txt
-python whatever.py
+
+python removesnpduplciate.py
 
 
 echo "chr peak_pos marker_pos coverage geno tis rep marker" > svcg.txt
