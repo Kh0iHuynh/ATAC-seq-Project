8d7b6dbe4287da8142cf491aed03753ce7ec887d
Kh0iHuynh <srongthiencos@gmail.com>
2021-01-11 07:54:56 -0800

    Update Causativesnpsvmaster.sh

--- Causativesnpsvmaster.sh
+++ Causativesnpsvmaster.sh
@@ -40,6 +40,15 @@ awk '{print $1,$2+1}' ci2.txt > lenint.txt
 python len.py
 
 
+sort snplenint.txt | uniq -c | awk '{ print $2,$3}' | sed 's/ /\t/g' > delete1.txt
+awk 'FNR==NR{a[$1] =$1;b[$2]= $2;next}($1 in a) && ($2 in b){ print $0;}' delete1.txt note.txt | sed 's/\t/ /g' > notesnplenint.txt
+sort snplengeno.txt | uniq -c | awk '{ print $2,$3}' | sed 's/ /\t/g' > delete2.txt
+awk 'FNR==NR{a[$1] =$1;b[$2]= $2;next}($1 in a) && ($2 in b){ print $0;}' delete2.txt note.txt | sed 's/\t/ /g' > notesnplengeno.txt
+sort SVlenint.txt | uniq -c | awk '{ print $2,$3}' | sed 's/ /\t/g' > delete3.txt
+awk 'FNR==NR{a[$1] =$1;b[$2]= $2;next}($1 in a) && ($2 in b){ print $0;}' delete3.txt note.txt | sed 's/\t/ /g' > noteSVlenint.txt
+sort SVlengeno.txt | uniq -c | awk '{ print $2,$3}' | sed 's/ /\t/g' > delete4.txt
+awk 'FNR==NR{a[$1] =$1;b[$2]= $2;next}($1 in a) && ($2 in b){ print $0;}' delete4.txt note.txt | sed 's/\t/ /g' > noteSVlengeno.txt
+
 python Lengthindex1.py
 python Lengthindex2.py
 
