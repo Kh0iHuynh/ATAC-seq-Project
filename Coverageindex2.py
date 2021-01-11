b3fe9aea0c0fcc7dd47ec2c1f40db39e1181ee5d
Kh0iHuynh <srongthiencos@gmail.com>
2021-01-11 07:50:14 -0800

    Update Coverageindex2.py

--- Coverageindex2.py
+++ Coverageindex2.py
@@ -8,7 +8,7 @@ else:
 file2 = open('temp2.txt', 'r')
 Lines2 = file2.readlines()
 
-
+##peak file that has snp,SV close
 file3 = open('notesnpcovgeno.txt', 'r')
 Lines3 = file3.readlines()
 
@@ -18,11 +18,11 @@ for lines3 in Lines3:
         min = int(lines3.split()[1]) - 250
         for lines2 in Lines2:
                 if lines2.split()[0] == lines3.split()[0] and int(lines2.split()[1]) < max and int(lines2.split()[1]) > min and lines2.split()[2] == lines3.split()[4]:
-                        f = open('scg2.txt', 'a')
+                        f = open('scg.txt', 'a')
                         print(lines3.split()[0],lines3.split()[1],lines2.split()[1],lines3.split()[2],lines3.split()[4],lines3.split()[5],lines3.split()[6],lines2.split()[3],sep ="\t" ,file=f)
                         f.close()
 
-
+import os
 if os.path.exists("sci.txt"):
   os.remove("sci.txt")
 else:
@@ -34,7 +34,7 @@ else:
 file2 = open('temp1.txt', 'r')
 Lines2 = file2.readlines()
 
-
+##peak file that has snp,SV close
 file3 = open('notesnpcovint.txt', 'r')
 Lines3 = file3.readlines()
 
@@ -44,11 +44,11 @@ for lines3 in Lines3:
         min = int(lines3.split()[1]) - 250
         for lines2 in Lines2:
                 if lines2.split()[0] == lines3.split()[0] and int(lines2.split()[1]) < max and int(lines2.split()[1]) > min and lines2.split()[2] == lines3.split()[4]:
-                        f = open('sci2.txt', 'a')
+                        f = open('sci.txt', 'a')
                         print(lines3.split()[0],lines3.split()[1],lines2.split()[1],lines3.split()[2],lines3.split()[4],lines3.split()[5],lines3.split()[6],lines2.split()[3],sep ="\t" ,file=f)
                         f.close()
 
-
+import os
 if os.path.exists("svcg.txt"):
   os.remove("svcg.txt")
 else:
@@ -71,11 +71,11 @@ for lines3 in Lines3:
         min = int(lines3.split()[1]) - 250
         for lines2 in Lines2:
                 if lines2.split()[0] == lines3.split()[0] and int(lines2.split()[1]) < max and int(lines2.split()[1]) > min and lines2.split()[2] == lines3.split()[4]:
-                        f = open('svcg2.txt', 'a')
+                        f = open('svcg.txt', 'a')
                         print(lines3.split()[0],lines3.split()[1],lines2.split()[1],lines3.split()[2],lines3.split()[4],lines3.split()[5],lines3.split()[6],lines2.split()[3],sep ="\t" ,file=f)
                         f.close()
 
-
+import os
 if os.path.exists("svci.txt"):
   os.remove("svci.txt")
 else:
@@ -97,7 +97,6 @@ for lines3 in Lines3:
         min = int(lines3.split()[1]) - 250
         for lines2 in Lines2:
                 if lines2.split()[0] == lines3.split()[0] and int(lines2.split()[1]) < max and int(lines2.split()[1]) > min and lines2.split()[2] == lines3.split()[4]:
-                        f = open('svci2.txt', 'a')
+                        f = open('svci.txt', 'a')
                         print(lines3.split()[0],lines3.split()[1],lines2.split()[1],lines3.split()[2],lines3.split()[4],lines3.split()[5],lines3.split()[6],lines2.split()[3],sep ="\t" ,file=f)
                         f.close()
-
